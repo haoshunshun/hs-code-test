3eaa9b4ca4832620aad409805fd94b6225c5b868
haoshunshun <haosh@bayescom.com>
2017-02-17 11:25:12 +0800

    add file

---
+++ bridge_temporary_code/sta_crid_ctr.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+#!-*- coding: utf-8 -*-
+
+import json
+clkcridDict={}
+wincridDict={}
+# statistic every crid's ctr
+file_name = open("/home/work/log/bridge/pv/hs_rlt","w+")
+for line in open("/home/work/log/bridge/pv/hs_win"):
+    try:
+        line = line.strip()
+        jsonObj = json.loads(line)
+        supplier = jsonObj['supplier']
+        crid = jsonObj['crid']
+        if wincridDict.get(crid,None)==None:
+            wins=1
+            wincridDict[crid]={"wins":wins}
+        else:
+            wincridDict[crid]["wins"]+=1
+    except:
+        continue
+for line in open("/home/work/log/bridge/pv/hs_click"):
+    try:
+        line = line.strip()
+        jsonObj = json.loads(line)
+        supplier = jsonObj['supplier']
+        crid = jsonObj['crid']
+        if clkcridDict.get(crid,None)==None:
+            clks=1
+            clkcridDict[crid]={"clks":clks}
+        else:
+            clkcridDict[crid]["clks"]+=1
+    except:
+        continue
+for key,v in wincridDict.items():
+    crid = key
+    wins = v["wins"]
+    if clkcridDict.get(key,None)!=None:
+        clks = clkcridDict[key]["clks"]
+        file_name.write("%s\t%s\t%s\t%s\n" % (key,v["wins"],clks,float(clks)/v["wins"]))
+    else:
+        clks = 0
+        file_name.write("%s\t%s\t%s\t%s\n" % (key,v["wins"],0,0.00))
