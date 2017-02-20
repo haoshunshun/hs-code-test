f800f6e013420baf3211dd81768a65536cc3e4c2
haoshunshun <haosh@bayescom.com>
2017-02-20 14:51:14 +0800

    add file

---
+++ sta_bridge_log/ip_sta/sta_pvip.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import json
+import os
+from ipip import IP
+pv_file=open('headpv.20161221.log')
+pv_ip_rlt=open('head_rlt','w+')
+IP.load(os.path.abspath('/Users/haoshun/hs-code-test/sta_bridge_log/ip_sta/ip_location.dat'))
+for line in pv_file:
+    jsonObj = json.loads(line)
+    try:
+        ip = jsonObj['input']['device']['ip']
+        iprlt = IP.find(ip)
+        pv_ip_rlt.write("%s\n" % (iprlt.encode('utf-8')))
+    except Exception as ex:
+        print ex.message
