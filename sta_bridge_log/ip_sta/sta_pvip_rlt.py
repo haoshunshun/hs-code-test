697b0146e01ebe7d881c282615568d66939f8fa8
haoshunshun <haosh@bayescom.com>
2017-02-21 15:30:01 +0800

    modify

--- sta_bridge_log/ip_sta/sta_pvip_rlt.py
+++ sta_bridge_log/ip_sta/sta_pvip_rlt.py
@@ -2,21 +2,22 @@
 #!-*- coding: utf-8 -*-
 
 staDict={}
-f_rlt = open('ip_result_finnal','w+')
-for line in open('head_rlt'):
-    lineStr = line.rstrip("\r\n").split("\t")
-    if len(lineStr)>=3:
-        con = lineStr[0]
-        pro = lineStr[1]
-        city = lineStr[2]
-        pk = "%s\t%s\t%s" % (con,pro,city)
-    else:
-        continue
-    if staDict.get(pk,None)==None:
-        staDict[pk]={"ip":1}
-    else:
-        staDict[pk]["ip"]+=1
-for k,v in staDict.items():
-    f_rlt.write("%s\t%s\n" % (k,v["ip"]))
-f_rlt.close()
-
+f_rlt = open('ip_result_finnal.csv','w+')
+try:
+    for line in open('head_rlt'):
+        lineStr = line.rstrip("\r\n").split("\t")
+        if len(lineStr)>=3:
+            con = lineStr[0]
+            pro = lineStr[1]
+            city = lineStr[2]
+            pk = "%s\t%s\t%s" % (con,pro,city)
+        else:
+            continue
+        if staDict.get(pk,None)==None:
+            staDict[pk]={"ip":1}
+        else:
+            staDict[pk]["ip"]+=1
+    for k,v in staDict.items():
+        f_rlt.write("%s\t%s\n" % (k,v["ip"]))
+finally:
+    f_rlt.close()
