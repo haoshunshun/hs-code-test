71a30d911f20b1519984e1c9294a91b4cc918a41
haoshunshun <haosh@bayescom.com>
2017-02-24 13:31:14 +0800

    download

---
+++ sta_bidmax_log/downloadlogfromBOS.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+#!-*- coding: utf-8 -*-
+bce bos cp bos:/hs-test/dataLog_2.0/ssp/BidMax/bid/20170222/bid.2017022212.log bid.2017022212.log
+bce bos cp bos:/hs-test/dataLog_2.0/ssp/BidMax/bid/20170222/bid.2017022213.log bid.2017022213.log
+bce bos cp bos:/hs-test/dataLog_2.0/ssp/BidMax/bid/20170222/bid.2017022214.log bid.2017022214.log
+bce bos cp bos:/hs-test/dataLog_2.0/ssp/BidMax/bid/20170222/bid.2017022215.log bid.2017022215.log
+bce bos cp bos:/hs-test/dataLog_2.0/ssp/BidMax/bid/20170222/bid.2017022216.log bid.2017022216.log
+bce bos cp bos:/hs-test/dataLog_2.0/ssp/BidMax/bid/20170222/bid.2017022217.log bid.2017022217.log
+bce bos cp bos:/hs-test/dataLog_2.0/ssp/BidMax/bid/20170222/bid.2017022218.log bid.2017022218.log
+bce bos cp bos:/hs-test/dataLog_2.0/ssp/BidMax/bid/20170222/bid.2017022219.log bid.2017022219.log
+bce bos cp bos:/hs-test/dataLog_2.0/ssp/BidMax/bid/20170222/bid.2017022220.log bid.2017022220.log
+bce bos cp bos:/hs-test/dataLog_2.0/ssp/BidMax/bid/20170222/bid.2017022221.log bid.2017022221.log
+bce bos cp bos:/hs-test/dataLog_2.0/ssp/BidMax/bid/20170222/bid.2017022222.log bid.2017022222.log
+bce bos cp bos:/hs-test/dataLog_2.0/ssp/BidMax/bid/20170222/bid.2017022223.log bid.2017022223.log
+
