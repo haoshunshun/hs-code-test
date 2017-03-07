88f204a1dc8340ddaa7a2eacda38894d02c9a862
haoshunshun <haosh@bayescom.com>
2017-03-07 15:29:00 +0800

    new file

--- bridge_temporary_code/sta_time_pv_win_clk.py
+++ bridge_temporary_code/sta_time_pv_win_clk.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python
 #!-*- coding: utf-8 -*-
 import json
-staDict={}
-pvList=[]
-winList=[]
+pvDict={}
+winDict={}
+clkDict={}
 for line in open("/home/work/log/bridge/pv/hs"):
     jsonObj = json.loads(line)
     action = jsonObj["action"]
     if action == "pv":
         pv_time = jsonObj["_time"]
         pv_auction = jsonObj["auction_id"]
-        pvList.append(pv_auction)
+        pvDict[pv_auction]={"time":pv_time}
     elif action == "win":
         win_time = jsonObj["_time"]
         win_auction = jsonObj["auction"]
-        winList.append(win_auction)
+        winDict[win_auction]={"time":win_time}
     else:
         clk_time = jsonObj["_time"]
         clk_auction = jsonObj["click_auction"]        
-        if clk_auction in winList and clk_auction in pvList:
-            print "%s\t%s\t%s\t%s" % (clk_auction,pv_time,win_time,clk_time)
-        else:
-            continue
+        clkDict[clk_auction]={"time":clk_time}
+for k,v in clkDict.items():
+    if k in winDict and k in pvDict:
+        print "%s\t%s\t%s\t%s" % (k,pvDict[k]["time"],winDict[k]["time"],clkDict[k]["time"])
+    else:
+        continue
