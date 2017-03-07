8c19d44075448f5048265488a7f0e0c3e65a39b8
haoshunshun <haosh@bayescom.com>
2017-03-07 11:01:08 +0800

    sta_ctr

--- bridge_temporary_code/sta_url_ctr.py
+++ bridge_temporary_code/sta_url_ctr.py
@@ -5,48 +5,58 @@ bidDict={}
 winDict={}
 clkDict={}
 staDict={}
+rltDict={}
 #bid message
-bid_path = '/Users/haoshun/hs-code-test/sample_data/bridge_bid'
+bid_path = '/home/work/log/bridge/pv/hs_bid_head'
 for bid_line in open(bid_path):
-	if "SUCCESS" in bid_line:
-		try:
-			bid_jsonObj = json.loads(bid_line)
-			auction_id = bid_jsonObj['auction_id']
-			if "impnative" in bid_line:
-				iurl = bid_jsonObj['rsp']['adsimp'][0]['impnative']['adslot']['image'][0]['iurl']
-			else:
-				iurl = bid_jsonObj['rsp']['adsimp'][0]['impbanner']['iurl']
-				bidDict[auction_id]={"iurl":iurl}
-		except:
-			continue
-	else:
-		continue
+    if "SUCCESS" in bid_line:
+        bid_jsonObj = json.loads(bid_line)
+        auction_id = bid_jsonObj['auction_id']
+        if "impnative" in bid_line:
+            iurl = bid_jsonObj['rsp']['adsimp'][0]['impnative']['adslot']['image'][0]['iurl']
+            bidDict[auction_id]={"iurl":iurl}
+        else:
+            continue
+    else:
+        continue
 #win message
-win_path = '/Users/haoshun/hs-code-test/sample_data/bridge_win'
+win_path = '/home/work/log/bridge/pv/hs_win_head'
 for win_line in open(win_path):
-	win_jsonObj = json.loads(win_line)
-	auction_id = win_jsonObj['auction']
-	winDict[auction_id]={"wins":"win"}
+    win_jsonObj = json.loads(win_line)
+    auction_id = win_jsonObj['auction']
+    winDict[auction_id]={"wins":"win"}
 #click message
-clk_path = '/Users/haoshun/hs-code-test/sample_data/bridge_click'
+clk_path = '/home/work/log/bridge/pv/hs_click_head'
 for clk_line in open(clk_path):
-	clk_jsonObj = json.loads(clk_line)
-	auction = clk_jsonObj['auction']
-	clkDict[auction]={"clks":"clk"}
+    clk_jsonObj = json.loads(clk_line)
+    auction = clk_jsonObj['auction']
+    clkDict[auction]={"clks":"clk"}
 #join bid win click and statistic
-clks = 0
-wins = 0
 for k,v in bidDict.items():
-	iurl = v["iurl"]
-	if winDict.get(k,None)!=None:
-		wins += 1
-		if clkDict.get(k,None)!=None:
-			clks += 1
-		else:
-			clks += 0
-	else:
-		wins += 0
-	staDict[iurl]={"wins":wins,"clks":clks}
+    iurl = v["iurl"]
+    if winDict.get(k,None)!=None and clkDict.get(k,None)!=None:
+        staDict[k]={"iurl":iurl,"bids":1,"wins":1,"clks":1}
+    elif winDict.get(k,None)!=None and clkDict.get(k,None)==None:
+        staDict[k]={"iurl":iurl,"bids":1,"wins":1,"clks":0}
+    elif winDict.get(k,None)==None and clkDict.get(k,None)!=None:
+        staDict[k]={"iurl":iurl,"bids":1,"wins":0,"clks":1}
+    else:
+        staDict[k]={"iurl":iurl,"bids":1,"wins":0,"clks":0}
 #read staDict
 for key,val in staDict.items():
-	print "%s\t%s\t%s" % (key,val["wins"],val["clks"])
+    auction = key
+    iurl = val["iurl"]
+    bids = val["bids"]
+    wins = val["wins"]
+    clks = val["clks"]
+    if rltDict.get(iurl,None)==None:
+        rltDict[iurl]={"bids":bids,"wins":wins}
+    else:
+        rltDict[iurl]["bids"]+=bids
+        rltDict[iurl]["wins"]+=wins
+for ke,va in rltDict.items():
+    iurl = ke
+    bids  = va["bids"]
+    wins = val["wins"]
+    print "%s\t%s\t%s" % (iurl,bids,wins)
+    print "%s\t%s\t%s\t%s" % (key,val["bids"],val["wins"],val["clks"])
